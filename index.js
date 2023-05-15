a839c648ebb833c57734cb97a91e3490c4d15212
Пользователь <arzygul.imankulova.kk@gmail.com>
2023-05-16 00:49:31 +0600

    setHeader

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.get('/', (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.json("Server work")
