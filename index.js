b9813e304eb00f3def93bec778ec55af61fd0849
Пользователь <arzygul.imankulova.kk@gmail.com>
2023-05-16 01:04:49 +0600

    frontend work

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    
     res.json("Server work")
 });
 
