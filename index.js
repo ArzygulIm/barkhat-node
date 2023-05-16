fd6932f813f5a8d8b2a56a7bd51ba10e7410f699
Пользователь <arzygul.imankulova.kk@gmail.com>
2023-05-16 20:09:30 +0600

    refactor: cahnge cors config

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ mongoose.connect('mongodb+srv://admin:barkhat123@cluster0.dc9nwne.mongodb.net/ba
 const app = express();
 
 app.use(cors({
-    origin: '*'
+    origin: ['http://localhost:3000']
 }));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
