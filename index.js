2452ccbce1447356275ae9727d595653e71e31f4
Пользователь <arzygul.imankulova.kk@gmail.com>
2023-05-23 19:26:19 +0600

    add all to deploy

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ mongoose.connect('mongodb+srv://admin:barkhat123@cluster0.dc9nwne.mongodb.net/ba
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', "https://barkhat.surge.sh"],
+    origin: ["*"],
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000", "https://barkhat.surge.sh");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.header(
         "Access-Control-Allow-Headers",
